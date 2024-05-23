eea29944c8403e29fbed04ab0104fafd58124df3
xukai <kyle2542736@gmail.com>
2024-05-23 13:31:57 +0800

    create project

---
+++ src/main/java/org/xiangan/fruitshopweb/FruitShopWebApplication.java
@@ -0,0 +1,13 @@
+package org.xiangan.fruitshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitShopWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xiangan/fruitshopweb/FruitShopWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xiangan.fruitshopweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
